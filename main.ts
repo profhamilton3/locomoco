7f7d1646945444f1dcbed7a74e5a873c6686a832
T. Hamilton <profhamilton3@gmail.com>
2024-10-29 21:57:57 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    wuKong.setMotorSpeed(wuKong.MotorList.M1, 50)
+input.onButtonPressed(Button.A, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.InBackground)
+    wuKong.setMotorSpeed(wuKong.MotorList.M1, -50)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
     wuKong.stopMotor(wuKong.MotorList.M1)
 })
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.UntilDone)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.InBackground)
